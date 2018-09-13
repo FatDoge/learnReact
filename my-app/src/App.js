c004a0ed03cc58fc12ec0e8a2e4e22639c95c4d1
fatdoge <704274491@qq.com>
2018-09-13 18:09:54 +0800

    react

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import CommentApp from './components/CommentApp'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <CommentApp/>
+      </div>
+    );
+  }
+}
+
+export default App;
