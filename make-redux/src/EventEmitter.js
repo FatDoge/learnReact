0bb1eb70403acf7fb64ca4a8d961fbd738b53b7f
fatdoge <704274491@qq.com>
2018-09-14 20:03:20 +0800

    reducer

---
+++ make-redux/src/EventEmitter.js
@@ -0,0 +1,50 @@
+class EventEmitter{
+  constructor(){
+    this.listeners={}
+  }
+  on(eventName,func){
+    if(!this.listeners[eventName]){
+      this.listeners[eventName]=[].concat(func)
+    }else{
+      this.listeners[eventName].push(func)
+    }
+  }
+
+  emit(eventName,...rest){
+    if(this.listeners[eventName]){
+      this.listeners[eventName].map(event=>{
+        event(...rest)
+      })
+    }
+  }
+  off(eventName,func){
+    if(this.listeners[eventName]){
+      const index = this.listeners[eventName].indexOf(func)
+      if(index!=-1){
+        this.listeners[eventName].splice(index,1)
+      }
+    }
+  }
+}
+const emitter = new EventEmitter()
+
+const sayHi = (name) => console.log(`Hello ${name}`)
+const sayHi2 = (name) => console.log(`Good night, ${name}`)
+
+emitter.on('hi', sayHi)
+emitter.on('hi', sayHi2)
+console.log(emitter.listeners['hi'][0])
+emitter.emit('hi', 'ScriptOJ')
+// => Hello ScriptOJ
+// => Good night, ScriptOJ
+
+emitter.off('hi', sayHi)
+emitter.emit('hi', 'ScriptOJ')
+// => Good night, ScriptOJ
+
+const emitter2 = new EventEmitter()
+emitter2.on('hi', (name, age) => {
+  console.log(`I am ${name}, and I am ${age} years old`)
+})
+emitter2.emit('hi', 'Jerry', 12)
+// => I am Jerry, and I am 12 years old
---
+++ make-redux/src/index.js
@@ -0,0 +1,78 @@
+function createStore(stateChanger){
+  let state=null
+  const listeners=[]
+  const subscribe=(listener)=>listeners.push(listener)
+  const getState=()=>state
+  const dispatch=(action)=>{
+    state=stateChanger(state,action)
+    listeners.forEach((listener)=>listener())
+  }
+  dispatch({})
+  return {getState,dispatch,subscribe}
+}
+function stateChanger(state,action){
+  if(!state){
+    state = {
+      title: {
+        text: 'React.js 小书',
+        color: 'red',
+      },
+      content: {
+        text: 'React.js 小书内容',
+        color: 'blue'
+      }
+    }
+  }
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      return {
+        ...state,
+        title:{
+          ...state.title,
+          text:action.text
+        }
+      }
+    case 'UPDATE_TITLE_COLOR':
+      return {
+        ...state,
+        title:{
+          ...state.title,
+          color:action.color
+        }
+      }
+    default:
+    return state
+  }
+}
+
+function renderApp(newAppState,oldAppState={}){
+  if(newAppState===oldAppState)return
+  console.log('app render...')
+  renderTitle(newAppState.title,oldAppState.title)
+  renderContent(newAppState.content,oldAppState.content)
+}
+function renderTitle(newTitle, oldTitle = {}) {
+  if(newTitle===oldTitle)return
+  console.log('title render...')
+  const titleDOM=document.querySelector('#title')
+  titleDOM.innerHTML=newTitle.text
+  titleDOM.style.color=newTitle.color
+}
+function renderContent(newContent, oldContent = {}) {
+  if(newContent===oldContent)return
+  console.log('content render...')
+  const contentDOM=document.querySelector('#content')
+  contentDOM.innerHTML=newContent.text
+  contentDOM.style.color=newContent.color
+}
+
+const store=createStore(stateChanger)
+let oldAppState=store.getState()
+store.subscribe(()=>{
+  const newAppState=store.getState()
+  renderApp(newAppState,oldAppState)
+  oldAppState=newAppState
+})
+renderApp(store.getState()) // 首次渲染页面
+store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
+store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
